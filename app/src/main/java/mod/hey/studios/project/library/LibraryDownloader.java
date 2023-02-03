abe69da6c4baebd4f7b104005b1dbaa3b5153a3f
DereckySany <leirecky@gmail.com>
2023-02-03 02:45:41 -0300

    Update LibraryDownloader.java

--- app/src/main/java/mod/hey/studios/project/library/LibraryDownloader.java
+++ app/src/main/java/mod/hey/studios/project/library/LibraryDownloader.java
@@ -899,7 +899,7 @@ public class LibraryDownloader {
             if (success) {
                 // make a Toast 
                 bB.a(context, "The library has been downloaded and imported to local libraries successfully.", 10).show();
-                Snackbar snackbar = Snackbar.a(context, "Library: " + libName.toString(), -2 ); /* BaseTransientBottomBar.LENGTH_INDEFINITE */
+                Snackbar snackbar = Snackbar.a(progressDialog, "Library: " + libName.toString(), -2 ); /* BaseTransientBottomBar.LENGTH_INDEFINITE */
                 snackbar.a(Helper.getResString(R.string.common_word_show), v -> {
                     snackbar.c();
                     // to imprementation go to library add recently
