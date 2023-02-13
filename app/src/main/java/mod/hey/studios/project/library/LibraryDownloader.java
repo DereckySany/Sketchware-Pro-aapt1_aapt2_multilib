b1b3b50989be73de96f7bd71edeb6b179cd7ab11
DereckySany <leirecky@gmail.com>
2023-02-13 17:23:25 -0300

    Update LibraryDownloader.java
    removed some commands that have not been working perfectly

--- app/src/main/java/mod/hey/studios/project/library/LibraryDownloader.java
+++ app/src/main/java/mod/hey/studios/project/library/LibraryDownloader.java
@@ -427,18 +427,17 @@ public class LibraryDownloader {
 
             } else if (tool.equals("R8")) {
                 // R8
-                ArrayList<String> cmd = new ArrayList<>();
-                
+                ArrayList<String> cmd = new ArrayList<>();           
                 cmd.add("--release");
-                cmd.add("--lib");
-                cmd.add(new File(BuiltInLibraries.EXTRACTED_COMPILE_ASSETS_PATH, "android.jar").getAbsolutePath());
-                cmd.add("--classpath");
-                cmd.add(new File(BuiltInLibraries.EXTRACTED_COMPILE_ASSETS_PATH, "core-lambda-stubs.jar").getAbsolutePath());
                 // Output
                 cmd.add("--output");
-                cmd.add(new File(_path).getParentFile().getAbsolutePath());        
+                cmd.add(new File(_path).getParentFile().getAbsolutePath());                  
+                // cmd.add("--lib");
+                // cmd.add(new File(BuiltInLibraries.EXTRACTED_COMPILE_ASSETS_PATH, "android.jar").getAbsolutePath());
+                // cmd.add("--classpath");
+                // cmd.add(new File(BuiltInLibraries.EXTRACTED_COMPILE_ASSETS_PATH, "core-lambda-stubs.jar").getAbsolutePath());
                 // Input
-                cmd.add("--input");
+                // cmd.add("--input");
                 cmd.add(_path);
                 // run D8 with list commands
                 R8.main(cmd.toArray(new String[0]));
@@ -456,8 +455,8 @@ public class LibraryDownloader {
             Main.clearInternTables();
             // dx
             // 6.3.0 fix1
+            // "--dex",
             Main.main(new String[]{
-                    "--dex",
                     "--debug",
                     "--verbose",
                     "--keep-classes",
