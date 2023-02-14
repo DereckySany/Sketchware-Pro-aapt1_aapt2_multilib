da64e2f39eaab156c5e6824d4a50d942b5e02bfd
DereckySany <leirecky@gmail.com>
2023-02-14 17:20:00 -0300

    clean R8Compiler

--- app/src/main/java/mod/hey/studios/project/library/R8Compiler.java
+++ app/src/main/java/mod/hey/studios/project/library/R8Compiler.java
@@ -11,27 +11,25 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import mod.agus.jcoderz.lib.FileUtil;
-import mod.hey.studios.project.ProjectSettings;
 
 public class R8Compiler {
     private final String inputFilePath;
     private final String outputFilePath;
 
     public R8Compiler(String inputFilePath, String outputFilePath) {
-        // Construtor que inicializa as variáveis de entrada e saída do arquivo.
+        /* Construtor que inicializa as variáveis de entrada e saída do arquivo. */
         this.inputFilePath = inputFilePath;
         this.outputFilePath = outputFilePath;
     }
 
     public void compile() throws IOException {
-        // Método que compila o arquivo de entrada com o R8.
-        ProjectSettings settings = 0;
+        /* Método que compila o arquivo de entrada com o R8.*/
         Path inputFile = null;
         Path outputFile = null;
         Path ANDROID_PROGUARD_RULES_PATH = null;
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            // Verifica se a versão do Android é maior ou igual ao Android 8.0 (Oreo).
-            // Se for, o caminho para o arquivo de entrada é obtido com o Paths.get().
+            /* Verifica se a versão do Android é maior ou igual ao Android 8.0 (Oreo).
+             Se for, o caminho para o arquivo de entrada é obtido com o Paths.get().*/
             inputFile = Paths.get(inputFilePath);
             outputFile = Paths.get(outputFilePath);
             ANDROID_PROGUARD_RULES_PATH = Paths.get(FileUtil.getExternalStorageDir() + "/.sketchware/libs/android-proguard-rules.pro");
@@ -39,16 +37,16 @@ public class R8Compiler {
 
         R8Command command;
         try {
-            // Tenta criar o objeto R8Command, que define as configurações para a compilação.
+            /* Tenta criar o objeto R8Command, que define as configurações para a compilação.*/
             command = R8Command.builder()
                 .addProgramFiles(inputFile)
-                .setMinApiLevel(settings.getMinSdkVersion())
+                .setMinApiLevel(21)
                 .setOutput(outputFile, OutputMode.DexIndexed)
                 .addProguardConfigurationFiles(ANDROID_PROGUARD_RULES_PATH)
                 .setMode(CompilationMode.RELEASE)
                 .build();
         } catch (CompilationFailedException e) {
-            // Captura a exceção CompilationFailedException e lança uma nova exceção do tipo RuntimeException.
+            /* Captura a exceção CompilationFailedException e lança uma nova exceção do tipo RuntimeException.*/
             throw new RuntimeException(e);
         }
         try {
