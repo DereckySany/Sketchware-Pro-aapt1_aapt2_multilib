941aefbb12bf6ac12d1794a6e54658e054122499
DereckySany <leirecky@gmail.com>
2023-02-13 22:38:23 -0300

    fix R8Compiler

--- app/src/main/java/mod/hey/studios/project/library/R8Compiler.java
+++ app/src/main/java/mod/hey/studios/project/library/R8Compiler.java
@@ -6,15 +6,20 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import com.android.tools.r8.CompilationMode;
+import com.android.tools.r8.OutputMode;
 import com.android.tools.r8.R8;
-import com.android.tools.r8.R8Command.*;
+import com.android.tools.r8.R8Command;
+import com.android.tools.r8.origin.Origin;
 import com.android.tools.r8.shaking.ProguardRuleParserException;
 
 import mod.hey.studios.project.proguard.ProguardHandler;
+import mod.hey.studios.project.ProjectSettings;
 
 public class R8Compiler {
     private final String inputFilePath;
     private final String outputFilePath;
+    public ProjectSettings settings;
 
     public R8Compiler(String inputFilePath, String outputFilePath) {
         this.inputFilePath = inputFilePath;
@@ -26,8 +31,10 @@ public class R8Compiler {
         Path outputFile = Paths.get(outputFilePath);
         R8Command command = R8Command.builder()
             .addProgramFiles(inputFile)
+            .setMinApiLevel(settings.getMinSdkVersion())
             .setOutput(outputFile, R8Command.OutputMode.DexIndexed)
             .addProguardConfiguration(ProguardHandler.ANDROID_PROGUARD_RULES_PATH, new String[0])
+            .setMode(CompilationMode.RELEASE)
             .build();
         R8.run(command);
     }
