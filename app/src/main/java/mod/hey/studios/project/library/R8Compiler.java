cce406d2f3908e82818ac3420a93d3231b4ee508
DereckySany <leirecky@gmail.com>
2023-02-14 14:18:59 -0300

    addProguardConfigurationFiles

--- app/src/main/java/mod/hey/studios/project/library/R8Compiler.java
+++ app/src/main/java/mod/hey/studios/project/library/R8Compiler.java
@@ -10,13 +10,12 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import mod.agus.jcoderz.lib.FileUtil;
 import mod.hey.studios.project.ProjectSettings;
 
 public class R8Compiler {
-    private final String ANDROID_PROGUARD_RULES_PATH;
     private final String inputFilePath;
     private final String outputFilePath;
-    private final ProjectSettings settings;
 
     public R8Compiler(String inputFilePath, String outputFilePath) {
         // Construtor que inicializa as variáveis de entrada e saída do arquivo.
@@ -26,28 +25,26 @@ public class R8Compiler {
 
     public void compile() throws IOException {
         // Método que compila o arquivo de entrada com o R8.
-        
+        ProjectSettings settings = null;
         Path inputFile = null;
+        Path outputFile = null;
+        Path ANDROID_PROGUARD_RULES_PATH = null;
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
             // Verifica se a versão do Android é maior ou igual ao Android 8.0 (Oreo).
             // Se for, o caminho para o arquivo de entrada é obtido com o Paths.get().
             inputFile = Paths.get(inputFilePath);
-        }
-        Path outputFile = null;
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            // Verifica se a versão do Android é maior ou igual ao Android 8.0 (Oreo).
-            // Se for, o caminho para o arquivo de saída é obtido com o Paths.get().
             outputFile = Paths.get(outputFilePath);
+            ANDROID_PROGUARD_RULES_PATH = Paths.get(FileUtil.getExternalStorageDir() + "/.sketchware/libs/android-proguard-rules.pro");
         }
-        ANDROID_PROGUARD_RULES_PATH = FileUtil.getExternalStorageDir() + "/.sketchware/libs/android-proguard-rules.pro";
+
         R8Command command;
         try {
             // Tenta criar o objeto R8Command, que define as configurações para a compilação.
             command = R8Command.builder()
                 .addProgramFiles(inputFile)
-                .setMinApiLevel(21)
+                .setMinApiLevel(settings.getMinSdkVersion())
                 .setOutput(outputFile, OutputMode.DexIndexed)
-                .setProguardConfig(new File(ANDROID_PROGUARD_RULES_PATH))
+                .addProguardConfigurationFiles(ANDROID_PROGUARD_RULES_PATH)
                 .setMode(CompilationMode.RELEASE)
                 .build();
         } catch (CompilationFailedException e) {
