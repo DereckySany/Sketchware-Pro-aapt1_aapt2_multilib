eecc0dd5af0aa9bb643539ca90414ddfffd017b5
DereckySany <leirecky@gmail.com>
2023-02-15 17:53:46 -0300

    fix R8Compiler args

--- app/src/main/java/mod/hey/studios/project/library/R8Compiler.java
+++ app/src/main/java/mod/hey/studios/project/library/R8Compiler.java
@@ -1,24 +1,20 @@
 package mod.hey.studios.project.library;
 
+import android.os.Build;
+
 import com.android.tools.r8.CompilationFailedException;
 import com.android.tools.r8.CompilationMode;
 import com.android.tools.r8.OutputMode;
 import com.android.tools.r8.R8;
 import com.android.tools.r8.R8Command;
-import com.android.tools.r8.R8Command.Builder;
-import com.android.tools.r8.utils.AndroidApiLevel;
-import com.android.tools.r8.utils.FileUtils;
 
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import mod.agus.jcoderz.lib.FileUtil;
 import mod.jbk.build.BuiltInLibraries;
 
 public class R8Compiler {
@@ -35,31 +31,32 @@ public class R8Compiler {
         /* Método que compila o arquivo de entrada com o R8.*/
         Path inputFile = null;
         Path outputFile = null;
+        // Caminho dos arquivos
+        Path androidJarPath = null;
+        Path coreStubsJarPath = null;
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
             /* Verifica se a versão do Android é maior ou igual ao Android 8.0 (Oreo).
              Se for, o caminho para o arquivo de entrada é obtido com o Paths.get().*/
             inputFile = Paths.get(inputFilePath);
             outputFile = Paths.get(outputFilePath);
+            coreStubsJarPath = Paths.get(String.valueOf(BuiltInLibraries.EXTRACTED_COMPILE_ASSETS_PATH), "core-lambda-stubs.jar");
+            androidJarPath = Paths.get(String.valueOf(BuiltInLibraries.EXTRACTED_COMPILE_ASSETS_PATH), "android.jar");
         }
-                // Caminho dos arquivos
-        Path coreStubsJarPath = Paths.get(BuiltInLibraries.EXTRACTED_COMPILE_ASSETS_PATH, "core-lambda-stubs.jar");
-        Path androidJarPath = Paths.get(BuiltInLibraries.EXTRACTED_COMPILE_ASSETS_PATH, "android.jar");
 
         // Lista de classpaths
         List<String> classpath = Arrays.asList(coreStubsJarPath.toString());
 
         // Lista de libs
-        List<String> library = Arrays.asList(androidJarPath.toString());
+        Collection<Path> library = Arrays.asList(androidJarPath,coreStubsJarPath);
 
         R8Command command;
         try {
             /* Tenta criar o objeto R8Command, que define as configurações para a compilação.*/
             command = R8Command.builder()
                 .setMode(CompilationMode.RELEASE)
-                .setIntermediate(true)
                 .setDisableDesugaring(true)
                 .setMinApiLevel(26)
-                .setClasspathFiles(classpath)
+                .setAndroidPlatformBuild(true)
                 .addLibraryFiles(library)
                 .setOutput(outputFile, OutputMode.DexIndexed)
                 .addProgramFiles(inputFile)
