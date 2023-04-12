4c591a2d8dae6a3851a85fad7b360b2e36cf51a8
DereckySany <leirecky@gmail.com>
2023-04-12 17:24:21 -0300

    add: imageButton to filter Collection Blocks on LogicEditorDrawer;

--- app/src/main/java/com/besome/sketch/editor/LogicEditorDrawer.java
+++ app/src/main/java/com/besome/sketch/editor/LogicEditorDrawer.java
@@ -4,8 +4,10 @@ import static mod.SketchwareUtil.getDip;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -16,6 +18,8 @@ import com.besome.sketch.lib.ui.CustomScrollView;
 import com.sketchware.remod.R;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 import a.a.a.Us;
 import a.a.a.wB;
@@ -26,6 +30,7 @@ public class LogicEditorDrawer extends LinearLayout {
 
     private LinearLayout favorite;
     private CustomScrollView scrollView;
+    private boolean ascendingOrder = true;
 
     public LogicEditorDrawer(Context context) {
         super(context);
@@ -50,6 +55,7 @@ public class LogicEditorDrawer extends LinearLayout {
         ((TextView) findViewById(R.id.tv_block_collection)).setText(Helper.getResString(R.string.logic_editor_title_block_collection));
         favorite = findViewById(R.id.layout_favorite);
         scrollView = findViewById(R.id.scv);
+        ImageButton ascendingOrdernation = findViewById(R.id.sort_collection);
 
         CardView tools = findViewById(R.id.new_button);
         tools.setOnClickListener(v -> {
@@ -57,10 +63,60 @@ public class LogicEditorDrawer extends LinearLayout {
             intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
             ((LogicEditorActivity) getContext()).startActivityForResult(intent, 463);
         });
-    }
 
+        findViewById(R.id.sort_collection).setOnClickListener(v -> {
+            sortCollections();
+            Drawable drawable = getContext().getDrawable(ascendingOrder ? R.drawable.selector_ic_expand_more_24 : R.drawable.selector_ic_expand_less_24);
+            ascendingOrdernation.setImageDrawable(drawable);
+        });
+    }
+    private void sortCollections() {
+        ArrayList<Us> collections = new ArrayList<>();
+        for (int i = 0; i < favorite.getChildCount(); i++) {
+            View childAt = favorite.getChildAt(i);
+            if (childAt instanceof Us) {
+                collections.add((Us) childAt);
+            }
+        }
+
+        if (ascendingOrder) {
+            Collections.sort(collections, new Comparator<Us>() {
+                @Override
+                public int compare(Us o1, Us o2) {
+                    return o1.T.compareToIgnoreCase(o2.T);
+                }
+            });
+            ascendingOrder = false;
+        } else {
+            Collections.sort(collections, new Comparator<Us>() {
+                @Override
+                public int compare(Us o1, Us o2) {
+                    return o2.T.compareToIgnoreCase(o1.T);
+                }
+            });
+            ascendingOrder = true;
+        }
+
+        favorite.removeAllViews();
+        for (Us collection : collections) {
+            favorite.addView(collection);
+            View view = new View(getContext());
+            view.setLayoutParams(new LinearLayout.LayoutParams(
+                    1,
+                    (int) getDip(8)));
+            favorite.addView(view);
+        }
+    }
     public void a() {
         favorite.removeAllViews();
+    }
+
+    public boolean z(){
+        if (ascendingOrder) {
+            return scrollView.fullScroll(View.FOCUS_DOWN);
+        } else {
+            return scrollView.fullScroll(View.FOCUS_UP);
+        }
     }
 
     public View a(String str, ArrayList<BlockBean> arrayList) {
@@ -75,7 +131,7 @@ public class LogicEditorDrawer extends LinearLayout {
                     (int) getDip(8)));
             favorite.addView(view);
         }
-
+//        z();
         return collectionBlock;
     }
 
